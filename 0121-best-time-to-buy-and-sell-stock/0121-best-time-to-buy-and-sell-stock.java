758660d2cd61bfd79ced712c6a630c7f51525986
Sadhna Mall <142137558+sadhna786@users.noreply.github.com>
2024-07-18 23:32:28 +0530

    Time: 2 ms (70.2%), Space: 61.6 MB (52.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,8 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length == 0){
-            return 0;
-        }
+
        int min = Integer.MAX_VALUE;
        int profit =0;
        for(int i=0 ; i< prices.length ; i++){
